29df340f4050e19498301654f9edd75300d2ec2d
José Manuel S <s5po@gmx.com>
2022-06-05 20:09:27 -0500

    Added upload empty file test

--- tests/test_anomaly_data_viz.py
+++ tests/test_anomaly_data_viz.py
@@ -1,7 +1,15 @@
 """Test for anomaly_data_viz app"""
+
 from anomaly_data_viz import __version__
+from src.anomaly_data_viz.utils import upload_file
 
 
 def test_version():
     """Test app version"""
     assert __version__ == "0.1.0"
+
+
+def test_load_empty_file():
+    """Test load empty string file"""
+    dataset = upload_file("", "")
+    assert dataset.shape == (0, 0)
