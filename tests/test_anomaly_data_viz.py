761d2862e2a8f68440390ca1038d5465447e30ba
José Manuel S <s5po@gmx.com>
2022-06-05 21:40:04 -0500

    Added non empty file test

--- tests/test_anomaly_data_viz.py
+++ tests/test_anomaly_data_viz.py
@@ -1,5 +1,11 @@
 """Test for anomaly_data_viz app"""
 
+import base64
+import csv
+from io import StringIO
+
+import pandas as pd
+
 from anomaly_data_viz import __version__
 from src.anomaly_data_viz.utils import upload_file
 
@@ -13,3 +19,15 @@ def test_load_empty_file():
     """Test load empty string file"""
     dataset = upload_file("", "")
     assert dataset.shape == (0, 0)
+
+
+def test_load_non_empty_file():
+    """Test load non_empty string file"""
+    data = [["complex", "3i"], ["real", 15], ["rational", 2.5], ["integer", 2]]
+    file_input = StringIO()
+    csv.writer(file_input).writerows(data)
+    encoded_input = base64.b64encode(file_input.getvalue().encode()).decode()
+    payload = "text/csv;base64," + encoded_input
+    dataset = upload_file(payload, "numbers.csv")
+    assert isinstance(dataset, pd.core.frame.DataFrame)
+    assert dataset.shape[0] > 0
